fab6ad5caa44bb90ac925020a26d6e4283b5da91
Aaron Huisinga <aaron@platform.marketing>
2017-05-03 15:29:38 -0500

    Bump to version 1.0.2
    1. Better support for tracking Facebook conversions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -146,35 +146,25 @@ jQuery(function ($) {
                         var retargeting = $('#retargeting').val();
                         var conversion = $('#conversion').val();
                         if (retargeting != '') {
-                            (function () {
-                                var _fbq = window._fbq || (window._fbq = []);
-                                if (!_fbq.loaded) {
-                                    var fbds = document.createElement('script');
-                                    fbds.async = true;
-                                    fbds.src = '//connect.facebook.net/en_US/fbds.js';
-                                    var s = document.getElementsByTagName('script')[0];
-                                    s.parentNode.insertBefore(fbds, s);
-                                    _fbq.loaded = true;
-                                }
-                                _fbq.push(['addPixelId', retargeting]);
-                            })();
-                            window._fbq = window._fbq || [];
-                            window._fbq.push(['track', 'PixelInitialized', {}]);
+                            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+                            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                            document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+                            fbq('init', retargeting);
+                            fbq('track', 'PageView');
                         }
                         if (conversion != '') {
-                            (function () {
-                                var _fbq = window._fbq || (window._fbq = []);
-                                if (!_fbq.loaded) {
-                                    var fbds = document.createElement('script');
-                                    fbds.async = true;
-                                    fbds.src = '//connect.facebook.net/en_US/fbds.js';
-                                    var s = document.getElementsByTagName('script')[0];
-                                    s.parentNode.insertBefore(fbds, s);
-                                    _fbq.loaded = true;
-                                }
-                            })();
-                            window._fbq = window._fbq || [];
-                            window._fbq.push(['track', conversion, {'value': '0.00', 'currency': 'USD'}]);
+                            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+                            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                            document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+                            fbq('init', conversion);
+                            fbq('track', 'PageView');
+                            fbq('track', 'Lead');
                         }
                     }, 1000);
                 }
