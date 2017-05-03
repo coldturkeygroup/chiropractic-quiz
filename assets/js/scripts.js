0d639b71e53c06a364f4dfe2f40d11a1fcd8e326
Aaron Huisinga <aaron@platform.marketing>
2017-05-03 10:40:28 -0500

    Clean up some JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,173 +7,170 @@ jQuery(function ($) {
             $('.btn-primary').removeAttr('disabled');
         });
 
-        // Email Validation
-        if (ChiroQuiz.mailgun !== undefined && ChiroQuiz.mailgun !== '') {
-            $('#email').mailgun_validator({
-                api_key: ChiroQuiz.mailgun,
-                in_progress: function () {
-                    $('#email').parent().removeClass('has-warning has-error');
-                    $(".mailcheck-suggestion").remove();
-                    $("[type=submit]").addClass("disabled").attr("disabled", "disabled");
-                },
-                success: function (data) {
-                    $('#email').after(get_suggestion_str(data['is_valid'], data['did_you_mean']));
-                },
-                error: function () {
-                    $("[type=submit]").removeClass("disabled").removeAttr("disabled");
-                }
-            });
-        }
-        // Parse Mailgun Responses
-        function get_suggestion_str(is_valid, alternate) {
-            if (is_valid) {
-                if (alternate) {
-                    $('#email').parent().addClass('has-warning');
-                    return '<div class="mailcheck-suggestion help-block">Did you mean <a href="#">' + alternate + '</a>?</div>';
-                }
-                if ($('#form-clicked').length) {
-                    $('form').unbind().submit();
-                    $("[type=submit]").addClass("disabled").attr("disabled", "disabled");
-                } else {
-                    $("[type=submit]").removeClass("disabled").removeAttr("disabled");
-                }
-
-                return;
-            }
-            $('#email').parent().addClass('has-error');
-            if (alternate) {
-                return '<div class="mailcheck-suggestion help-block">This email is invalid. Did you mean <a href="#">' + alternate + '</a>?</div>';
-            }
-            return '<div class="mailcheck-suggestion help-block">This email is invalid.</div>';
-        }
-
-        $(".form-group").on("click", ".mailcheck-suggestion a", function (e) {
-            e.preventDefault();
-            $("#email").val($(this).text());
-            $("[type=submit]").removeClass("disabled").removeAttr("disabled");
-            $(".mailcheck-suggestion").remove();
-        });
-        $('form').submit(function (e) {
-            e.preventDefault();
-            $(this).after('<input type="hidden" id="form-clicked" value="true">');
-            $('#email').trigger('focusout');
-        });
-
         // Start quiz
-        $('#start-quiz').click(function () {
-            nextStep();
-            $('.footer').addClass('animated fadeOutDown');
-            $('.footer-quiz').addClass('animated fadeInUpBig').show();
+        nextStep();
+        $('.footer').addClass('animated fadeOutDown');
+        $('.footer-quiz').addClass('animated fadeInUpBig').show();
 
-            setTimeout(function () {
-                $('.footer').hide();
-            }, 300);
+        setTimeout(function () {
+            $('.footer').hide();
+        }, 300);
+    });
 
-            return false;
-        });
-
-        // Allow users to go back
-        $('#quiz-back').click(function () {
-            previousStep();
-
-            return false;
-        });
-
-        // styling for quiz choices
-        $('.page input[type=radio]').each(function () {
-            var self = $(this),
-                label = self.next();
-
-            label.remove();
-            self.iCheck({
-                checkboxClass: 'icheckbox_line',
-                radioClass: 'iradio_line',
-                insert: '<label><i class="fa fa-fw"></i> ' + label.text() + '</label>'
-            });
-        });
-
-        // progress quiz when option is picked
-        $('.page input[type=radio]').on('ifClicked', function () {
-            setTimeout(function () {
-                nextStep()
-            }, 300);
-
-            return false;
-        });
-
-        // Show quiz results modal
-        $('#get-results').click(function () {
-            $('#quiz-results').modal('show');
-
-            return false;
-        });
-
-        // Submit quiz results
-        $('#submit-results').click(function (e) {
-            e.preventDefault();
-
-            if (stepVerify('quiz') == 0) {
-                var form = $('#chiro-quiz');
-
-                $.ajax({
-                    type: 'POST',
-                    url: ChiroQuiz.ajaxurl,
-                    data: form.serialize(),
-                    dataType: 'json',
-                    async: true,
-                    success: function (response) {
-                        $('#first_name_2').val($('#first_name').val());
-                        $('#email_2').val($('#email').val());
-                        $('#quiz-results .modal-body').html('<h2><i class="fa fa-check-circle"></i> <br> <small>Success!</small>');
-                        $('#user_id').val(response.user_id);
-                        $('#quiz-back').hide();
-
-                        setTimeout(function () {
-                            $('#quiz-results').modal('hide');
-                            $('#offer').html('<h2 class="quiz-completed"><i class="fa fa-check-circle"></i> <br> <strong>You scored ' + response.score + '/88</strong><br><small>' + response.feedback + '</small></h2>');
-                            if (typeof $('#valuator-link').val() != 'undefined') {
-                                $('#offer').html('<h2 class="quiz-completed"><i class="fa fa-check-circle"></i> <br> <strong>You scored ' + response.score + '/88</strong><br><small>' + response.feedback + '</small></h2> <a href="' + $('#valuator-link').val() + '" class="btn btn-primary btn-lg" id="show-offer">Click Here To See What Your Home Is Worth <br> <small>(Based On Official Data of Recently Sold Listings In Your Area)</small></a>');
-                            }
-                            $('.quiz-page').animate({'padding-top': '6%'}, 500);
-
-                            var retargeting = $('#retargeting').val(),
-                                conversion = $('#conversion').val();
-                            if (retargeting != '') {
-                                (function () {
-                                    var _fbq = window._fbq || (window._fbq = []);
-                                    if (!_fbq.loaded) {
-                                        var fbds = document.createElement('script');
-                                        fbds.async = true;
-                                        fbds.src = '//connect.facebook.net/en_US/fbds.js';
-                                        var s = document.getElementsByTagName('script')[0];
-                                        s.parentNode.insertBefore(fbds, s);
-                                        _fbq.loaded = true;
-                                    }
-                                    _fbq.push(['addPixelId', retargeting]);
-                                })();
-                                window._fbq = window._fbq || [];
-                                window._fbq.push(['track', 'PixelInitialized', {}]);
-                            }
-                            if (conversion != '') {
-                                (function () {
-                                    var _fbq = window._fbq || (window._fbq = []);
-                                    if (!_fbq.loaded) {
-                                        var fbds = document.createElement('script');
-                                        fbds.async = true;
-                                        fbds.src = '//connect.facebook.net/en_US/fbds.js';
-                                        var s = document.getElementsByTagName('script')[0];
-                                        s.parentNode.insertBefore(fbds, s);
-                                        _fbq.loaded = true;
-                                    }
-                                })();
-                                window._fbq = window._fbq || [];
-                                window._fbq.push(['track', conversion, {'value': '0.00', 'currency': 'USD'}]);
-                            }
-                        }, 1000);
-                    }
-                });
+    // Email Validation
+    if (ChiroQuiz.mailgun !== undefined && ChiroQuiz.mailgun !== '') {
+        $('#email').mailgun_validator({
+            api_key: ChiroQuiz.mailgun,
+            in_progress: function () {
+                $('#email').parent().removeClass('has-warning has-error');
+                $(".mailcheck-suggestion").remove();
+                $("[type=submit]").addClass("disabled").attr("disabled", "disabled");
+            },
+            success: function (data) {
+                $('#email').after(get_suggestion_str(data['is_valid'], data['did_you_mean']));
+            },
+            error: function () {
+                $("[type=submit]").removeClass("disabled").removeAttr("disabled");
             }
         });
+    }
+    // Parse Mailgun Responses
+    function get_suggestion_str(is_valid, alternate) {
+        if (is_valid) {
+            if (alternate) {
+                $('#email').parent().addClass('has-warning');
+                return '<div class="mailcheck-suggestion help-block">Did you mean <a href="#">' + alternate + '</a>?</div>';
+            }
+            if ($('#form-clicked').length) {
+                $('form').unbind().submit();
+                $("[type=submit]").addClass("disabled").attr("disabled", "disabled");
+            } else {
+                $("[type=submit]").removeClass("disabled").removeAttr("disabled");
+            }
+
+            return;
+        }
+        $('#email').parent().addClass('has-error');
+        if (alternate) {
+            return '<div class="mailcheck-suggestion help-block">This email is invalid. Did you mean <a href="#">' + alternate + '</a>?</div>';
+        }
+        return '<div class="mailcheck-suggestion help-block">This email is invalid.</div>';
+    }
+
+    $(".form-group").on("click", ".mailcheck-suggestion a", function (e) {
+        e.preventDefault();
+        $("#email").val($(this).text());
+        $("[type=submit]").removeClass("disabled").removeAttr("disabled");
+        $(".mailcheck-suggestion").remove();
+    });
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $(this).after('<input type="hidden" id="form-clicked" value="true">');
+        $('#email').trigger('focusout');
+    });
+
+    // Allow users to go back
+    $('#quiz-back').click(function () {
+        previousStep();
+
+        return false;
+    });
+
+    // styling for quiz choices
+    $('.page input[type=radio]').each(function () {
+        var self = $(this),
+            label = self.next();
+
+        label.remove();
+        self.iCheck({
+            checkboxClass: 'icheckbox_line',
+            radioClass: 'iradio_line',
+            insert: '<label><i class="fa fa-fw"></i> ' + label.text() + '</label>'
+        });
+    });
+
+    // progress quiz when option is picked
+    $('.page input[type=radio]').on('ifClicked', function () {
+        setTimeout(function () {
+            nextStep()
+        }, 300);
+
+        return false;
+    });
+
+    // Show quiz results modal
+    $('#get-results').click(function () {
+        $('#quiz-results').modal('show');
+
+        return false;
+    });
+
+    // Submit quiz results
+    $('#submit-results').click(function (e) {
+        e.preventDefault();
+
+        if (stepVerify('quiz') == 0) {
+            var form = $('#chiro-quiz');
+
+            $.ajax({
+                type: 'POST',
+                url: ChiroQuiz.ajaxurl,
+                data: form.serialize(),
+                dataType: 'json',
+                async: true,
+                success: function (response) {
+                    $('#first_name_2').val($('#first_name').val());
+                    $('#email_2').val($('#email').val());
+                    $('#quiz-results .modal-body').html('<h2><i class="fa fa-check-circle"></i> <br> <small>Success!</small>');
+                    $('#user_id').val(response.user_id);
+                    $('#quiz-back').hide();
+
+                    setTimeout(function () {
+                        $('#quiz-results').modal('hide');
+                        $('#offer').html('<h2 class="quiz-completed"><i class="fa fa-check-circle"></i> <br> <strong>You scored ' + response.score + '/88</strong><br><small>' + response.feedback + '</small></h2>');
+                        if (typeof $('#valuator-link').val() != 'undefined') {
+                            $('#offer').html('<h2 class="quiz-completed"><i class="fa fa-check-circle"></i> <br> <strong>You scored ' + response.score + '/88</strong><br><small>' + response.feedback + '</small></h2> <a href="' + $('#valuator-link').val() + '" class="btn btn-primary btn-lg" id="show-offer">Click Here To See What Your Home Is Worth <br> <small>(Based On Official Data of Recently Sold Listings In Your Area)</small></a>');
+                        }
+                        $('.quiz-page').animate({'padding-top': '6%'}, 500);
+
+                        var retargeting = $('#retargeting').val(),
+                            conversion = $('#conversion').val();
+                        if (retargeting != '') {
+                            (function () {
+                                var _fbq = window._fbq || (window._fbq = []);
+                                if (!_fbq.loaded) {
+                                    var fbds = document.createElement('script');
+                                    fbds.async = true;
+                                    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+                                    var s = document.getElementsByTagName('script')[0];
+                                    s.parentNode.insertBefore(fbds, s);
+                                    _fbq.loaded = true;
+                                }
+                                _fbq.push(['addPixelId', retargeting]);
+                            })();
+                            window._fbq = window._fbq || [];
+                            window._fbq.push(['track', 'PixelInitialized', {}]);
+                        }
+                        if (conversion != '') {
+                            (function () {
+                                var _fbq = window._fbq || (window._fbq = []);
+                                if (!_fbq.loaded) {
+                                    var fbds = document.createElement('script');
+                                    fbds.async = true;
+                                    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+                                    var s = document.getElementsByTagName('script')[0];
+                                    s.parentNode.insertBefore(fbds, s);
+                                    _fbq.loaded = true;
+                                }
+                            })();
+                            window._fbq = window._fbq || [];
+                            window._fbq.push(['track', conversion, {'value': '0.00', 'currency': 'USD'}]);
+                        }
+                    }, 1000);
+                }
+            });
+        }
     });
 
     function stepVerify(step) {
@@ -271,7 +268,7 @@ jQuery(function ($) {
                                     n.callMethod ?
                                         n.callMethod.apply(n, arguments) : n.queue.push(arguments)
                                 };
-                                if (!f._fbq)f._fbq = n;
+                                if (!f._fbq) f._fbq = n;
                                 n.push = n;
                                 n.loaded = !0;
                                 n.version = '2.0';
@@ -324,11 +321,3 @@ jQuery(function ($) {
         $('.progress-bar').attr('aria-valuenow', progress).css('width', progress + '%');
     }
 });
-
-var delay = (function () {
-    var timer = 0;
-    return function (callback, ms) {
-        clearTimeout(timer);
-        timer = setTimeout(callback, ms);
-    };
-})();
